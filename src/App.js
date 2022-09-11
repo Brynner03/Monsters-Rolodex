a693e8b4bf085f3a4ae54fedf8ea33fa8279c8f1
Brynner03 <bventura03@hotmail.com>
2022-09-11 16:36:17 -0400

    Mapping data to website instead of hard-coding it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react'
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,36 +7,27 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: 'Brynner', lastName: 'Ventura'},
-      company: 'Google'
-    }
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Jacky',
+        },
+      ]
+    };
   }
 
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi, my name is {this.state.name.firstName} {this.state.name.lastName}, and I work at {this.state.company}!</p>
-        <button 
-        onClick={() => {
-          this.setState((state, props) => {
-            return {
-              name: { firstName: 'Magelyn', lastName: 'Ortiz'},
-              company:  'a school' 
-            };
-          },
-          () => {
-            console.log(this.state)
-          } 
-          );
-
-        }} 
-        >
-          Change Name 
-          </button>
-      </header>
-    </div>
+      {
+        this.state.monsters.map(() => {})
+      }
+      </div>
   );
 }
 

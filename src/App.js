27eea05cd755de913e19b97016f037f1dbecd125
Brynner03 <bventura03@hotmail.com>
2022-09-11 20:22:19 -0400

    Working on onChange search bar

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor')
   }
 
   componentDidMount() {
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) => 
@@ -25,8 +27,12 @@ class App extends Component {
     }
 
   render() {
+    console.log('render')
     return (
       <div className="App">
+      <input className='search-box' type='search' placeholder= 'search monsters' onChange={(event) => { 
+        console.log(event)
+      }} />
       {
         this.state.monsters.map((monster) => {
           return (

3912589adc67775b3e379abc3edf94f77937c7c1
Brynner03 <bventura03@hotmail.com>
2022-09-11 20:04:09 -0400

    Fetching data from API using .fetch

--- src/App.js
+++ src/App.js
@@ -7,25 +7,33 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-        },
-        {
-          name: 'Frank',
-        },
-        {
-          name: 'Jacky',
-        },
-      ]
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => 
+      this.setState(
+        () => {
+          return {monsters: users}
+        },
+        () => {
+          console.log(this.state)
+        }))
+    }
 
   render() {
     return (
       <div className="App">
       {
-        this.state.monsters.map(() => {})
+        this.state.monsters.map((monster) => {
+          return (
+            <div key= {monster.id} >
+             <h1> {monster.name} </h1> 
+            </div>)
+        })
       }
       </div>
   );

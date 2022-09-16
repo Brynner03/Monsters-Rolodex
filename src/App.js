0081fb0de9d0c28a7d27d4208296d55b7a52e0ba
Brynner03 <bventura03@hotmail.com>
2022-09-15 20:51:20 -0400

    Optizimized statesfor better readability

--- src/App.js
+++ src/App.js
@@ -39,8 +39,10 @@ class App extends Component {
   render() {
     console.log('render')
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -49,7 +51,7 @@ class App extends Component {
         className='search-box' 
         type='search' 
         placeholder= 'search monsters' 
-        onChange={this.onSearchChange} 
+        onChange={onSearchChange} 
       />
       {
         filteredMonsters.map((monster) => {
